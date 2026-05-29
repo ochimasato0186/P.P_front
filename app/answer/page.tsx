fe370b5684b3c756f983358a3c49e6ff3c4123d1
ochimasato0186 <masato.mario@icloud.com>
2026-05-29 20:11:03 +0900

    結果画面作成

--- app/answer/page.tsx
+++ app/answer/page.tsx
@@ -15,20 +15,42 @@ export default function AnswerPage() {
         <main style={{ padding: 20 }}>
           <TimeBox hour={hour} width={260} height={100}>問題文</TimeBox>
           <TimeBox hour={hour} width={260} height={200}>模範解答、回答</TimeBox>
-          <div style={{ width: "100%", display: "flex", justifyContent: "center", marginTop: 40 }}>
-            <TimeButton
-              label="NEXT"
-              hour={hour}
-              style={{
-                minWidth: 200,
-                minHeight: 56,
-                borderRadius: 999,
-                fontSize: 28,
-                color: "#222",
-                border: "none",
-                letterSpacing: 2,
-              }}
-            />
+
+          {/* 表 横3×縦11 */}
+          <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center", marginTop: 40 }}>
+            <table style={{ borderCollapse: "collapse", width: 320 }}>
+              <tbody>
+                {[...Array(11)].map((_, rowIdx) => (
+                  <tr key={rowIdx}>
+                    {[...Array(3)].map((_, colIdx) => (
+                      <td
+                        key={colIdx}
+                        style={{ border: "1px solid #888", width: 100, height: 32, textAlign: "center" }}
+                      >
+                        {/* セル内容 */}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div style={{ marginTop: 32 }}>
+              <TimeButton
+                label="NEXT"
+                hour={hour}
+                style={
+                  {
+                    minWidth: 200,
+                    minHeight: 56,
+                    borderRadius: 999,
+                    fontSize: 28,
+                    color: "#222",
+                    border: "none",
+                    letterSpacing: 2,
+                  }
+                }
+              />
+            </div>
           </div>
         </main>
         {frameOn && <Road insideFrame={true} />}
--- app/date/page.tsx
+++ app/date/page.tsx
@@ -2,15 +2,46 @@
 import Road from "../../components/Road";
 import { useContext } from "react";
 import { FrameContext } from "../../components/PhoneFrame";
+import TimeBox from "../../components/TimeBox";
+import TimeButton from "../../components/TimeButton";
 
 export default function DatePage() {
   const frameOn = useContext(FrameContext);
+  const hour = new Date().getHours();
   return (
     <>
       <div style={{ position: "relative", width: "100%", height: "100%" }}>
         <main style={{ padding: 20 }}>
-          <h1>結果画面</h1>
-          <p>ここにアプリの内容を書く</p>
+          {/* 表 横3×縦11 カラム: 問題No、問題、回答 */}
+          <div style={{ width: "100%", overflowX: "auto", marginTop: 40, marginBottom: 32 }}>
+            <table style={{ borderCollapse: "collapse", width: "100%", minWidth: 240, tableLayout: "fixed" }}>
+              <colgroup>
+                <col style={{ width: "40px" }} />
+                <col style={{ width: "auto" }} />
+                <col style={{ width: "40px" }} />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th style={{ border: "1px solid #000", height: 32, background: "#f0f0f0", minWidth: 0, maxWidth: 40 }}>No</th>
+                  <th style={{ border: "1px solid #000", height: 32, background: "#f0f0f0" }}>問題</th>
+                  <th style={{ border: "1px solid #000", height: 32, background: "#f0f0f0", minWidth: 0, maxWidth: 40 }}>回答</th>
+                </tr>
+              </thead>
+              <tbody>
+                {[...Array(11)].map((_, rowIdx) => (
+                  <tr key={rowIdx}>
+                    <td style={{ border: "1px solid #000", textAlign: "center", minWidth: 0, maxWidth: 40 }}>{rowIdx + 1}</td>
+                    <td style={{ border: "1px solid #000", textAlign: "center" }}></td>
+                    <td style={{ border: "1px solid #000", textAlign: "center", minWidth: 0, maxWidth: 40 }}></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <TimeBox hour={hour} width={300} height={100} style={{ marginBottom: 4 }}>正答率</TimeBox>
+          <div style={{ width: "100%", display: "flex", justifyContent: "center", marginTop: 0 }}>
+            <TimeButton label="RETRY" hour={hour} />
+          </div>
         </main>
         {frameOn && <Road insideFrame={true} />}
       </div>
